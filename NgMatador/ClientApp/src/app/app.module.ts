8783c2d3e0130e039939dab1b7a6e13eed0a07f7
Brian Mikinski <brian.mikinski@gmail.com>
2020-03-21 23:09:22 -0500

    update to angular 9

--- NgMatador/ClientApp/src/app/app.module.ts
+++ NgMatador/ClientApp/src/app/app.module.ts
@@ -12,7 +12,14 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MainNav2Component } from './main-nav2/main-nav2.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AmoritComponent } from './amorit/amorit.component'
 
 
